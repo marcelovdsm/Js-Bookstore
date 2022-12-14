348c762dcd88e304cf218a12e2a146a29635d16d
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2022-12-14 14:05:37 +0000

    Finalizado a parte de script do projeto

---
+++ App.js
@@ -0,0 +1,74 @@
+const Book = require("./classes/Book")
+const Database = require("./Database")
+const Author = require("./entities/Author")
+const Order = require("./entities/Order")
+const Poster = require("./entities/Poster")
+const User = require("./entities/User")
+
+module.exports = class App {
+  static #database = new Database()
+
+  createUser(name, email, password) {
+    const user = new User(name, email, password)
+    App.#database.saveUser(user)
+  }
+
+  getUsers() {
+    return App.#database.find('users')
+  }
+
+  createAuthor(name, nationality, bio) {
+    const author = new Author(name, nationality, bio)
+    App.#database.saveAuthor(author)
+  }
+
+  getAuthors() {
+    return App.#database.find('authors')
+  }
+
+  createBooks(title, synopsis, genre, pages, author, description, price, inStock) {
+    const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+    App.#database.saveBook(book)
+  }
+
+  addBook(bookName, quantity) {
+    App.#database.addBooksToStock(bookName,quantity)
+  }
+
+  getBooks() {
+    return App.#database.find('books')
+  }
+
+  createPosters(name, description, height, width, price, inStock) {
+    const poster = new Poster(name, description, height, width, price, inStock)
+    App.#database.savePoster(poster)
+  }
+
+  addPoster(posterName, quantity) {
+    App.#database.addPostersToStock(posterName, quantity)
+  }
+
+  getPosters() {
+    return App.#database.find('posters')
+  }
+
+  createOrder(items, user) {
+    const order = new Order(items, user)
+    App.#database.saveOrder(order)
+    order.data.items.forEach(({ product, quantity }) => {
+      if(product instanceof Book) {
+        App.#database.removeBooksFromStock(product.name, quantity)
+      } else if(product instanceof Poster) {
+        App.#database.removePostersFromStock(product.name, quantity)
+      }
+    })
+  }
+
+  getOrders() {
+    return App.#database.find('orders')
+  }
+
+  showDatabase() {
+    App.#database.showStorage()
+  }
+}

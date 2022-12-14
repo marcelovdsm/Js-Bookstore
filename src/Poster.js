a42d21efd929757cccecfdc1ebf8ab8a8ede8896
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2022-12-14 12:25:30 +0000

    Realizado a criação das classes usadas no projeto

---
+++ src/Poster.js
@@ -0,0 +1,9 @@
+const Product = require("./Products");
+
+module.exports = class Poster extends Product {
+  constructor(name, description, height, width, price, inStock = 0) {
+    super(name, description, price, inStock)
+    this.height = height
+    this.width = width
+  }
+}
